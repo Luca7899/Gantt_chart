074aeca7b28ea472f87509fac62331c8e631d5d0
Luca <luca@auto-intern.de>
2019-10-28 14:46:05 +0100

    add property sourcefile to the class File_reader

--- file_reader.py
+++ file_reader.py
@@ -1,12 +1,13 @@
 import csv
 
 class File_reader:
-    def __init__(self):
+    def __init__(self, sourcefile):
+        self.sourcefile = sourcefile
         self.allinputdata = [] 
 
 
-    def read_csv(self, csvfile):
-        with open(csvfile) as csvDataFile:
+    def read_csv(self):
+        with open(self.sourcefile) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
             for row in csvReader:
                 self.allinputdata.append() 
